a31c9521daa51981248cb10998000dc939d4c128
sonirupal <rupalsoni07@gmail.com>
2024-05-09 11:34:28 +0530

    calculator done

---
+++ src/Calculator.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import './calc.css';
+
+const Calculator = () => {
+    const[calcInput, setCalcInput] = useState("")
+    const[showResult,setShowResult] = useState("")
+    const handleCalcClick = (value) =>{
+        console.log(value)
+        setCalcInput(calcInput+value)
+    }
+    const performCalc = () =>{
+        if(calcInput){
+            let result = eval(calcInput)
+            setShowResult(result)
+        }
+    }
+    const handleClear = () =>{
+        setCalcInput('')
+    }
+  return (
+    <div className='container'>
+      <h1>React Calculator</h1>
+      <input type='text' value={calcInput}/>
+        <div className='result'>{showResult}</div>
+        <div className='calc-grid'>
+            <button className='operator' onClick={() => handleCalcClick("7")}>7</button>
+            <button className='operator' onClick={() => handleCalcClick("8")}>8</button>
+            <button className='operator' onClick={() => handleCalcClick("9")}>9</button>
+            <button className='operator' onClick={() => handleCalcClick("+")}>+</button>
+            <button className='operator' onClick={() => handleCalcClick("4")}>4</button>
+            <button className='operator' onClick={() => handleCalcClick("5")}>5</button>
+            <button className='operator' onClick={() => handleCalcClick("6")}>6</button>
+            <button className='operator' onClick={() => handleCalcClick("-")}>-</button>
+            <button className='operator' onClick={() => handleCalcClick("1")}>1</button>
+            <button className='operator' onClick={() => handleCalcClick("2")}>2</button>
+            <button className='operator' onClick={() => handleCalcClick("3")}>3</button>
+            <button className='operator' onClick={() => handleCalcClick("*")}>*</button>
+            <button className='operator' onClick={() => handleClear()}>C</button>
+            <button className='operator' onClick={() => handleCalcClick("0")}>0</button>
+            <button className='operator' onClick={() => performCalc()}>=</button>
+            <button className='operator' onClick={() => handleCalcClick("/")}>/</button>
+        </div>
+    </div>
+  )
+}
+
+export default Calculator
